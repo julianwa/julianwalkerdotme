5395f3ea09c40f338e5457310d10849173fd456e
Julian Walker <julianreidwalker@gmail.com>
2010-10-24 21:13:23 -0700

    Added network links/images.

--- main.py
+++ main.py
@@ -14,20 +14,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+		template_values = {}
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, template_values))
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+	application = webapp.WSGIApplication([('/', MainHandler)],
                                          debug=True)
-    util.run_wsgi_app(application)
+	util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+	main()
